76acfc54776d83e5a363b35b9f84ce66ed1796ed
ChristyLin <31979820+ChristyLin@users.noreply.github.com>
2018-11-29 14:15:26 +0800

    Add files via upload

---
+++ Ch9/Ex9_1.py
@@ -0,0 +1,51 @@
+from serial import Serial
+import paho.mqtt.client as mqttClient
+from time import sleep
+import json
+
+AllowedActions = ['both', 'publish', 'subscribe']
+
+# Custom MQTT message callback
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to brocker")
+    else:
+        print("Connection failed")
+
+
+def on_publish(client, messageJson):
+    if mode == 'publish':
+        print('Published topic %s: %s\n' % (topic, messageJson))
+
+broker = "192.168.50.138"
+port = 31275
+clientId = "myRPi"
+topic = "RPi/PM25"
+mode = "publish"
+
+client = mqttClient.Client(clientId)
+client.on_connect = on_connect
+client.on_publish = on_publish
+print("try connect")
+client.connect(broker,port)
+sleep(2)
+
+loopCount = 0
+ser = Serial('/dev/ttyACM0', 9600, timeout=.5)
+
+while True:
+    if ser.inWaiting():
+        str = ser.readline().decode('utf8')[:-2]
+        end = str.find(' :')
+        if end > 0:
+            PM25 = str[:end]
+            message = {}
+            message['type'] = 'PM2.5'
+            message['value'] = PM25
+            message['device'] = 'RPi'
+            message['sequence'] = loopCount
+            messageJson = json.dumps(message)
+            client.publish(topic, messageJson)
+            sleep(2)
+            loopCount += 1
+
